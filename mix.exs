1784151eab8004632121abd2edb6a582dec5cfed
Peter Hizalev <peter@hizalev.com>
2018-12-01 10:53:03 -0800

    0.1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MachineGun.Mixfile do
   def project do
     [
       app: :machine_gun,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
